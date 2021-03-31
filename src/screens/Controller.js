1da846bf12b1ad31c681ec874a74214b0cdc4027
Prasanna Inamati <prasannainamati@gmail.com>
2021-03-31 15:34:26 +0530

    Made Changes to Header

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,26 +1,26 @@
-// import React, {Component} from 'react';
-// import Home from './home/Home';
-// import Details from './details/Details';
-// import Checkout from './checkout/Checkout';
-// import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React, {Component} from 'react';
+import Home from './home/Home';
+import Details from './details/Details';
+import Checkout from './checkout/Checkout';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-// class Controller extends Component {
-//     constructor() {
-//         super();
-//         this.baseUrl = "";
-//     }
-//     render() {
-//         return (
-//             <Router>
-//                 <div>
-//                     <Route exact path = "/" render = {(props) => <Home {...props} baseUrl = {this.baseUrl} />} />
-//                     <Route exact path = "/details" render = {(props) => <Details {...props} baseUrl = {this.baseUrl} />} />
-//                     <Route exact path = "/checkout" render = {(props) => <Checkout {...props} baseUrl = {this.baseUrl} />} />
-//                 </div>
+class Controller extends Component {
+    constructor() {
+        super();
+        this.baseUrl = "";
+    }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route exact path = "/" render = {(props) => <Home {...props} baseUrl = {this.baseUrl} />} />
+                    <Route exact path = "/details" render = {(props) => <Details {...props} baseUrl = {this.baseUrl} />} />
+                    <Route exact path = "/checkout" render = {(props) => <Checkout {...props} baseUrl = {this.baseUrl} />} />
+                </div>
 
-//             </Router>
-//         );
-//     }
-// }
+            </Router>
+        );
+    }
+}
 
-// export default Controller;
+export default Controller;
